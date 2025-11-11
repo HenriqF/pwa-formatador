bd9523a88a2f414bd0b376c54e796fc0cd4c3c48
Henrique Reinaldi <carlosofalmeida@gmail.com>
2025-11-11 19:32:20 -0300

    .
    .

--- testes/teste.spec.js
+++ testes/teste.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('pagina abriu', async ({ page }) => {
-  await page.goto('http://localhost:8080');
-  await expect(page).toHaveTitle(/Formatador/);
+  await page.goto('https://henriqf.github.io/formatador/');
+  await expect(page).toHaveTitle(/Document/);
 });
