46272228794f5cedd9a5564f299e930df7348a7c
Henrique Reinaldi <carlosofalmeida@gmail.com>
2025-11-12 11:00:36 -0300

    Update teste.spec.js

--- testes/teste.spec.js
+++ testes/teste.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('pagina abriu', async ({ page }) => {
   await page.goto('https://henriqf.github.io/pwa-formatador/');
-  await expect(page).toHaveTitle(/Document/);
+  await expect(page).toHaveTitle(/Formatador/);
 });
