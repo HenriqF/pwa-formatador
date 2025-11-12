691b47678ed18c6f78f4fadf4f1683391c677d36
Henrique Reinaldi <carlosofalmeida@gmail.com>
2025-11-12 10:42:50 -0300

    Delete testes directory

--- testes/teste.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('pagina abriu', async ({ page }) => {
-  await page.goto('https://henriqf.github.io/pwa-formatador/');
-  await expect(page).toHaveTitle(/Document/);
-});
-
